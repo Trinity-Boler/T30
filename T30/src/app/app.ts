554f067df7c9318c43f2a3fefb70bdac3426f6f2
TTQBqueen <ttboler@uncg.edu>
2025-11-03 10:33:28 -0500

    add compnets, login, dashborad, and menu

--- T30/src/app/app.ts
+++ T30/src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Menu } from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Login,
+    Menu
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('T30');
